b04a56393c6875a251c40abef347f32ec9dcf5fc
seropyh <69725611+seropyh@users.noreply.github.com>
2022-04-06 11:55:06 +0300

    First commit socs

---
+++ src/main/java/com/socs/socs/SocsApplication.java
@@ -0,0 +1,13 @@
+package com.socs.socs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/socs/socs/entity/Socks.java
@@ -0,0 +1,11 @@
+package com.socs.socs.entity;
+
+import lombok.Data;
+
+@Data
+public class Socks {
+    private long id;
+    private String color;
+    private int cottonPart;
+    private int quantity;
+}
